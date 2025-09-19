a58fc659adfdb359e898e73a507044860e661b60
drahmetacik <a.acik@icloud.com>
2025-09-19 09:29:09 +0100

    Add script demonstrating the use of subqueries in MySQL

---
+++ mysql_practice/06_subqueries_apply.py
@@ -0,0 +1,24 @@
+"""
+Using Subqueries and APPLY (MySQL does not support APPLY, but supports subqueries)
+- Scalar, correlated, and IN subqueries
+"""
+from scripts.db_config import HOST, USER, PASSWORD, DATABASE
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=DATABASE
+    )
+
+if __name__ == "__main__":
+    with get_connection() as conn:
+        cursor = conn.cursor(dictionary=True)
+        query = '''
+        SELECT * FROM employees WHERE salary > (SELECT AVG(salary) FROM employees);
+        '''
+        cursor.execute(query)
+        for row in cursor.fetchall():
+            print(row)
