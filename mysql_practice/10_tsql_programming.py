ed340995adb0fef622ca5114ee069d1f36d1f3c7
drahmetacik <a.acik@icloud.com>
2025-09-19 09:30:38 +0100

    Add script demonstrating the use of stored procedures in MySQL

---
+++ mysql_practice/10_tsql_programming.py
@@ -0,0 +1,31 @@
+"""
+Programming with Transact-SQL (T-SQL) in MySQL
+- Variables, control flow, stored procedures (MySQL style)
+"""
+from scripts.db_config import HOST, USER, PASSWORD, DATABASE
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=DATABASE
+    )
+
+if __name__ == "__main__":
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        # Example: Create and call a stored procedure
+        cursor.execute("""
+        DROP PROCEDURE IF EXISTS get_employee_count;
+        """)
+        cursor.execute("""
+        CREATE PROCEDURE get_employee_count()
+        BEGIN
+            SELECT COUNT(*) FROM employees;
+        END;
+        """)
+        cursor.callproc('get_employee_count')
+        for result in cursor.stored_results():
+            print(result.fetchall())
